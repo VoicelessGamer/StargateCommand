4a2f47f110656f58d0e4acd806702781cb618f2c
VoicelessGamer <adam.42@hotmail.co.uk>
2020-08-16 21:23:42 +0100

    Keyboard layout implemented

---
+++ Assets/Scripts/Missions/Input/AddressInput.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddressInput : MonoBehaviour {
+
+    public Keyboard keyboard;
+
+    public List<Sprite> symbolSprites;
+
+    public List<Button> inputChevrons;
+
+    private int activeSelection;
+
+    private int maxAddressSymbols = 7;
+
+    private int[] address;
+
+    // Start is called before the first frame update
+    void Start() {
+        clearAddress();
+    }
+
+    public void inputSymbol(int symbolIndex) {
+        inputChevrons[activeSelection].image.sprite = symbolSprites[symbolIndex];
+        inputChevrons[activeSelection].image.enabled = true;
+
+        if(address[activeSelection] != -1) {
+            keyboard.unlockGlyph(address[activeSelection]);
+        }
+
+        address[activeSelection] = symbolIndex;
+        keyboard.lockGlyph(address[activeSelection]);
+
+        if (activeSelection < (maxAddressSymbols - 1)) {
+            activeSelection++;
+        }
+    }
+
+    public void setActiveSelection(int index) {
+        activeSelection = index;
+        Debug.Log("Index set: " + index);
+    }
+
+    public void clearAddress() {
+        activeSelection = 0;
+        address = new int[maxAddressSymbols];
+        for (int i = 0; i < address.Length; i++) {
+            address[i] = -1;
+        }
+    }
+}
---
+++ Assets/Scripts/Missions/Input/Keyboard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keyboard : MonoBehaviour {
+    private List<GameObject> glyphKeys;
+
+    // Start is called before the first frame update
+    void Start() {
+        glyphKeys = new List<GameObject>();
+        foreach (Transform t in this.transform) {
+            if(t.gameObject.tag == "KeyboardGlyph") {
+                glyphKeys.Add(t.gameObject);
+            }
+        }
+    }
+
+    public void unlockAllGlyphs() {
+        foreach (GameObject go in glyphKeys) {
+            go.SetActive(true);
+        }
+    }
+
+    public void lockAllGlyphs() {
+        foreach (GameObject go in glyphKeys) {
+            go.SetActive(false);
+        }
+    }
+    public void unlockGlyph(int index) {
+        if(index > 0 && index < glyphKeys.Count) {
+            glyphKeys[index].SetActive(true);
+        }
+    }
+
+    public void lockGlyph(int index) {
+        if (index > 0 && index < glyphKeys.Count) {
+            glyphKeys[index].SetActive(false);
+        }
+    }
+}
